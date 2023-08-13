64a0288abc9a9257fa3057e11b29db6c21774afe
Anuragsingh691 <anuragshishodia691.gzb@gmail.com>
2023-08-13 18:54:25 +0530

    due card completed

--- app/src/main/java/com/example/cheqanimationdesign/MainActivity.kt
+++ app/src/main/java/com/example/cheqanimationdesign/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.cheqanimationdesign
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
 import android.view.animation.AnimationUtils
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.content.ContextCompat
 import com.example.cheqanimationdesign.databinding.ActivityMainBinding
 
@@ -19,8 +21,48 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        setViews()
         resizeTextView()
         setStatusBarColor(this, R.color.home_top_bg_color)
+    }
+
+    private fun setViews() {
+
+        // Axis card
+        binding.repaymentCard.icAxis.bankImg.setImageDrawable(
+            AppCompatResources.getDrawable(
+                this,
+                R.drawable.ic_axis
+            )
+        )
+        binding.repaymentCard.icAxis.axisHeadlineTxt.text = getString(R.string.axis_platinum_title)
+        binding.repaymentCard.icAxis.axisDescTxt.text= getString(R.string.credit_card_text)
+        binding.repaymentCard.icAxis.axisAmountHeadlineTxt.text = getString(R.string.axis_due_amount)
+        binding.repaymentCard.icAxis.axisAmountOverdueTxt.text = getString(R.string.overdue_by_1_day)
+
+        // Sbi card
+        binding.repaymentCard.icSbi.bankImg.setImageDrawable(
+            AppCompatResources.getDrawable(
+                this,
+                R.drawable.ic_sbi
+            )
+        )
+        binding.repaymentCard.icSbi.axisHeadlineTxt.text = getString(R.string.sbi_simply_title)
+        binding.repaymentCard.icSbi.axisDescTxt.text = getString(R.string.credit_card_text)
+        binding.repaymentCard.icSbi.axisAmountHeadlineTxt.text = getString(R.string.axis_due_amount)
+        binding.repaymentCard.icSbi.axisAmountOverdueTxt.text = getString(R.string.due_in_3_days)
+
+        // BOB card
+        binding.repaymentCard.icBob.bankImg.setImageDrawable(
+            AppCompatResources.getDrawable(
+                this,
+                R.drawable.ic_bob
+            )
+        )
+        binding.repaymentCard.icBob.axisHeadlineTxt.text = getString(R.string.bob_credit_title)
+        binding.repaymentCard.icBob.axisDescTxt.text = getString(R.string.credit_card_text)
+        binding.repaymentCard.icBob.noBillTxt.visibility = View.VISIBLE
+        binding.repaymentCard.icBob.noBillTxt.text = getString(R.string.no_bill_found)
     }
 
     private fun resizeTextView() {
